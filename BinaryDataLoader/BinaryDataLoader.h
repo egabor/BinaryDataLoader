6daba8c4bfccbaa55004c46670b4bfd28cd5b595
Mate Gujgiczer <mate.gujgiczer@icloud.com>
2016-08-15 14:49:30 +0200

    Initial Commit

---
+++ BinaryDataLoader/BinaryDataLoader.h
@@ -0,0 +1,19 @@
+//
+//  BinaryDataLoader.h
+//  BinaryDataLoader
+//
+//  Created by Gujgiczer Máté on 15/08/16.
+//  Copyright © 2016 gujci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BinaryDataLoader.
+FOUNDATION_EXPORT double BinaryDataLoaderVersionNumber;
+
+//! Project version string for BinaryDataLoader.
+FOUNDATION_EXPORT const unsigned char BinaryDataLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinaryDataLoader/PublicHeader.h>
+
+
