89218a432480bf32eb9fc5318f2fe33611bfb667
Mate Gujgiczer <mate.gujgiczer@icloud.com>
2016-09-19 09:07:19 +0200

    - swift3 support

--- BinaryDataLoader/BinaryDataLoader.swift
+++ BinaryDataLoader/BinaryDataLoader.swift
@@ -10,50 +10,54 @@ import UIKit
 
 public enum CachePolicy {
     /// Denys cache, loads all data
-    case NoCache
+    case noCache
     /// If founds it in cache returns it and stops
-    case AcceptCache
+    case acceptCache
     /// If founds it in cache returns the it than refreshes the cache
-    case RefreshCache
+    case refreshCache
     /// Returns tha cached data if ound furst, than returns the downloaded
-    case Newest
+    case newest
 }
 
 
 // MARK: - Protocol for generic methods
 public protocol BinaryLoadable {
-    init?(data: NSData)
+    static func create(with data: Data) -> Self?
 }
 
 // MARK: - default generic implementations
-extension UIImage: BinaryLoadable { }
-extension NSData: BinaryLoadable { }
+extension Data: BinaryLoadable {
+    public static func create(with data: Data) -> Data? {
+        return data
+    }
+}
 
-public class BinaryDataLoader {
-        
-    public func get<T: BinaryLoadable>(from url: String, cachePolicy: CachePolicy = .Newest, done: (data: T?) -> Void) {
+//MAKR: - Implementation
+open class BinaryDataLoader {
+    
+    open func get<T: BinaryLoadable>(from url: String, cachePolicy: CachePolicy = .newest, done: @escaping (_ data: T?) -> Void) {
         var foundInCache = false
         
-        if let cachedData = try? getPersistantData(by: url) where cachePolicy != .NoCache, let validData = cachedData {
+        if let cachedData = try? getPersistantData(by: url) , cachePolicy != .noCache, let validData = cachedData {
             foundInCache = true
-            dispatch_async(dispatch_get_main_queue()) { done(data: T(data: validData)) }
+            DispatchQueue.main.async { done(T.create(with: validData)) }
         }
         
-        if cachePolicy == .AcceptCache && foundInCache {
+        if cachePolicy == .acceptCache && foundInCache {
             return
         }
         
-        downloadData(NSURL(string:  url)!) { [weak self] (data) in
+        downloadData(URL(string:  url)!) { [weak self] (data) in
             guard let validData = data else {
-                done(data: nil)
+                done(nil)
                 return
             }
             
-            if (foundInCache && cachePolicy == .Newest) || !foundInCache {
-                dispatch_async(dispatch_get_main_queue()) { done(data: T(data: validData)) }
+            if (foundInCache && cachePolicy == .newest) || !foundInCache {
+                DispatchQueue.main.async { done(T.create(with: validData)) }
             }
             
-            if cachePolicy != .NoCache {
+            if cachePolicy != .noCache {
                 do {
                     try self?.savePersistant(validData, by: url)
                 }
@@ -67,38 +71,52 @@ public class BinaryDataLoader {
     public init() { }
 }
 
+//MARK: - Explicit UIImage support
+public extension BinaryDataLoader {
+    
+    public func get(from url: String, cachePolicy: CachePolicy = .newest, done: @escaping (_ data: UIImage?) -> Void) {
+        self.get(from: url, cachePolicy: cachePolicy) { (data: Data?) in
+            guard let validData = data else {
+                done(nil)
+                return
+            }
+            done(UIImage(data: validData))
+        }
+    }
+}
+
 //MARK: - Download
 extension BinaryDataLoader {
     
-    private func downloadData(url: NSURL, done: (data: NSData?) -> Void) {
-        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-        session.dataTaskWithURL(url) {
+    fileprivate func downloadData(_ url: URL, done: @escaping (_ data: Data?) -> Void) {
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        session.dataTask(with: url, completionHandler: {
             (data, response, error) in
             guard data != nil else {
-                done(data: nil)
+                done(nil)
                 return
             }
-            done(data: data)
-            }.resume()
+            done(data)
+            }) .resume()
     }
 }
 
 //MARK: - Cache
 extension BinaryDataLoader {
     
-    private var documentsURL: NSURL {
-        return NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+    fileprivate var documentsURL: URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
     }
     
-    private func savePersistant(data: NSData, by key: String) throws {
-        let cacheUrl = documentsURL.URLByAppendingPathComponent(key.stringByReplacingOccurrencesOfString("/", withString: "_"))
+    fileprivate func savePersistant(_ data: Data, by key: String) throws {
+        let cacheUrl = documentsURL.appendingPathComponent(key.replacingOccurrences(of: "/", with: "_"))
         
-        try data.writeToURL(cacheUrl, options: .AtomicWrite)
+        try data.write(to: cacheUrl, options: .atomicWrite)
     }
     
-    private func getPersistantData(by key: String) throws -> NSData? {
-        let cacheUrl = documentsURL.URLByAppendingPathComponent(key.stringByReplacingOccurrencesOfString("/", withString: "_"))
+    fileprivate func getPersistantData(by key: String) throws -> Data? {
+        let cacheUrl = documentsURL.appendingPathComponent(key.replacingOccurrences(of: "/", with: "_"))
         
-        return try NSData(contentsOfURL: cacheUrl, options: .DataReadingMappedIfSafe)
+        return try Data(contentsOf: cacheUrl, options: .mappedIfSafe)
     }
 }
